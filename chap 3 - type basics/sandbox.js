8018fd98c24419dbc3f353836a342eebef16d8d3
Talha Ayub <mtalhaayub752@gmail.com>
2023-01-30 09:06:20 +0100

    [TS - Chap3] - typescript types

---
+++ chap 3 - type basics/sandbox.js
@@ -0,0 +1,13 @@
+var character = 'Talha';
+var age = 27;
+var isFootballer = false;
+character = 'Khan';
+age = 39;
+isFootballer = true;
+console.log('character:', character);
+console.log('age:', age);
+console.log('isFootballer:', isFootballer);
+var circumference = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log('CircumFerence is: ', circumference(5));
---
+++ chap 3 - type basics/sandbox.ts
@@ -0,0 +1,17 @@
+let character = 'Talha';
+let age = 27;
+let isFootballer = false;
+
+character = 'Khan';
+age = 39;
+isFootballer = true;
+
+console.log('character:', character);
+console.log('age:', age);
+console.log('isFootballer:', isFootballer);
+
+const circumference = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log('CircumFerence is: ', circumference(5));
