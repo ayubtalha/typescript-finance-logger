403a740997fb05e680186b966296edf3e908a822
Talha Ayub <mtalhaayub752@gmail.com>
2023-01-31 09:00:45 +0100

    [TS - Chap4] - objects and arrays

---
+++ chap 4 - object and arrays/sandbox.js
@@ -0,0 +1,19 @@
+var arrays = ['Aye', 'Tal', 'Fat'];
+arrays.push('Khan');
+// arrays.push(3)
+arrays[0] = 'K';
+// arrays[1] = 3;
+console.log(arrays);
+var ninjaObject = {
+    name: 'Mario',
+    age: 27,
+    alive: true
+};
+ninjaObject.age = 30;
+// ninja.age="Khan"
+console.log(ninjaObject);
+// ninjaObject = {
+//   name: 'zen',
+//   // age: 27,
+//   alive: false,
+// };
