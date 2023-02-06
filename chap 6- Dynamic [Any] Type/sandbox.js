9886405fe25de04fc6bc03edd4ee3ccbb62d1c0a
Talha Ayub <mtalhaayub752@gmail.com>
2023-02-06 10:27:46 +0100

    [TS - Chap 6] - Dynamic [Any] Type

---
+++ chap 6- Dynamic [Any] Type/sandbox.js
@@ -0,0 +1,14 @@
+var age = 25;
+age = true;
+console.log(age);
+age = 'Talha';
+console.log(age);
+var mixed = [];
+mixed.push('Talha');
+mixed.push(24);
+console.log(mixed);
+var ninja;
+ninja = { name: 'Khan', age: 23 };
+console.log(ninja);
+ninja = { name: 23, age: 'Ta' };
+console.log(ninja);
---
+++ chap 6- Dynamic [Any] Type/sandbox.ts
@@ -0,0 +1,21 @@
+let age: any = 25;
+
+age = true;
+console.log(age);
+
+age = 'Talha';
+console.log(age);
+
+let mixed: any[] = [];
+
+mixed.push('Talha');
+mixed.push(24);
+
+console.log(mixed);
+
+let ninja: { name: any; age: any };
+
+ninja = { name: 'Khan', age: 23 };
+console.log(ninja);
+ninja = { name: 23, age: 'Ta' };
+console.log(ninja);
