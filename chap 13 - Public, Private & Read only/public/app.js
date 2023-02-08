d7e2e87ef359a4e0d228ee073fee9b517c53bda0
Talha Ayub <mtalhaayub752@gmail.com>
2023-02-08 07:35:04 +0100

    [TS - Chap 13] -  Functions Signatures

---
+++ chap 13 - Public, Private & Read only/public/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// classes
+
+var Invoice = /** @class */ (function () {
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
+  function Invoice(client, details, amount) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+  Invoice.prototype.format = function () {
+    return this.client + ' owes \u00A3' + this.amount + ' for ' + this.details;
+  };
+  return Invoice;
+})();
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(function (inv) {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+});
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
