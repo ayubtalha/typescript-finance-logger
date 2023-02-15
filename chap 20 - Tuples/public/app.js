df9b645f3c67a5c703c850ea9aea6be15d029819
Talha Ayub <mtalhaayub752@gmail.com>
2023-02-15 07:37:22 +0100

    [TS - Chap 20] - Tuples

---
+++ chap 20 - Tuples/public/app.js
@@ -0,0 +1,59 @@
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+const form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, 'end');
+});
+// ------------------------
+// GENERICS
+// ------------------------
+// const addUID = <T extends object>(obj: T) => {
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'Talha', age: 27 });
+// let docTwo = addUID('Hi');
+console.log(docOne.name);
+// console.log(docTwo);
+// ------------------------
+// ENUMS
+// ------------------------
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["HERO"] = 2] = "HERO";
+    ResourceType[ResourceType["PRODUCER"] = 3] = "PRODUCER";
+})(ResourceType || (ResourceType = {}));
+const docThree = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: 'Name of wind' },
+};
+const docFour = {
+    uid: 1,
+    resourceType: ResourceType.HERO,
+    data: { title: 'clash of titans' },
+};
+console.log(docThree, docFour);
